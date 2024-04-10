1b5beff5a6f5f419b1f4aad1104ec19cfbeef07c
alvega-g <alvarg@gmail.com>
2024-04-10 17:33:32 +0200

    started

---
+++ ex00/src/Fixed.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 17:08:51 by alvega-g          #+#    #+#             */
+/*   Updated: 2024/04/10 17:14:38 by alvega-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Fixed.hpp>
+
+Fixed::Fixed( void ) {
+	
+}
+
+Fixed::Fixed( const Fixed& other ) {
+	
+}
+
+Fixed& Fixed::operator=( const Fixed& other ) {
+	
+}
+
+Fixed::~Fixed( void ) {
+	
+}
---
+++ ex00/src/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alvega-g <alvega-g@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 16:46:19 by alvega-g          #+#    #+#             */
+/*   Updated: 2024/04/10 16:47:10 by alvega-g         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Fixed.hpp>
+
+int main( void ) {
+	
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	
+	return 0;
+}
